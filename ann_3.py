b149d3dc4bb15e033b6f197f79c91be421d71e8d
Saurabh Dubey <36095728+dusaurabh@users.noreply.github.com>
2019-10-02 18:23:23 +0530

    Update ann_3.py

--- ann_3.py
+++ ann_3.py
@@ -9,28 +9,18 @@ dataset = pd.read_csv('creditcard_1.csv')
 X = dataset.iloc[:, 0:30].values
 y = dataset.iloc[:, 30].values
 
-# Encoding categorical data
 
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-labelencoder_X_1 = LabelEncoder()
-X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
-labelencoder_X_2 = LabelEncoder()
-X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
-onehotencoder = OneHotEncoder(categorical_features = [1])
-X = onehotencoder.fit_transform(X).toarray()
-X = X[:, 1:]
-
-#Splitting the dataset into Training Set and Testing Set
+# Splitting the dataset into Training Set and Testing Set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 0)
 
-#Feature Scaling
+# Feature Scaling
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.fit_transform(X_test)
 
-# Part 2: Let's make an ANN
+# Part 2:  Make an ANN
 
 # Import the Keras libraries and packages
 import keras
@@ -39,27 +29,27 @@ from keras.layers import Dense
 from keras.layers import Dropout
 
 
-#Initializing the ANN
+# Initializing the ANN
 classifier = Sequential()
 
-#Adding the input layer and first hidden layer wih Dropout
+# Adding the input layer and first hidden layer wih Dropout
 classifier.add(Dense(output_dim = round(15.5), init = 'uniform', activation = 'relu', input_dim = 30 ))
 classifier.add(Dropout(p = 0.1 ))
 
-#Adding 2nd hidden layer
+# Adding 2nd hidden layer
 classifier.add(Dense(output_dim = round(15.5), init = 'uniform', activation = 'relu'))
 classifier.add(Dropout(p = 0.1 ))
 
-#Adding the ouput layer
+# Adding the ouput layer
 classifier.add(Dense(output_dim = 1, init = 'uniform', activation = 'sigmoid'))
 
 # Compling the ANN
 classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
-#Fitting the ANN to the Training set
+# Fitting the ANN to the Training set
 classifier.fit(X_train, y_train, batch_size = 32, nb_epoch = 25)
 
-#Part 3: Making the predictions and evaluating the model
+# Part 3: Making the predictions and evaluating the model
 y_pred = classifier.predict(X_test)
 y_pred = (y_pred > 0.5)
 
@@ -104,7 +94,7 @@ variance = accuracies.std()
 
 
 
-#Tuning the ANN
+# Tuning the ANN
 from keras.wrappers.scikit_learn import KerasClassifier
 from sklearn.model_selection import GridSearchCV
 from keras.models import Sequential
